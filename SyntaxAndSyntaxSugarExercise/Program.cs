2cac4f967d5783fd2a89320740e63bd5f2dd0c06
muhammad <m.doug98@hotmail.com>
2024-08-14 14:39:45 -0400

    syntax sugar exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Sugar();
+            
+        }
+
+        static void Sugar()
+        {
+            
+            var answer = 27;
+            var answer2 = 29;
+           // if (answer < answer2)
+            //{ 
+                //response = $"{answer} is less than {answer2}";
+            //}
+           // else
+            //{
+                //response = $"{answer} is greater than or equal to {answer2} ";
+                
+                
+            //}
+
+              var response = answer < answer2
+                ? $"{answer} is less than {answer2}"
+                : $"{answer} is greater than {answer2}";
+            Console.WriteLine(response);
         }
     }
 }
